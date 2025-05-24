3de649b54de63a39c7343ec373ed0d2ffcbd1887
Aibyn1234 <aibyn.nugmanov@yandex.ru>
2025-05-24 23:59:59 +0500

    add attack

--- logic.py
+++ logic.py
@@ -1,50 +1,111 @@
-from random import randint
 import requests
+from random import randint
 
 class Pokemon:
     pokemons = {}
 
-    def __init__(self, pokemon_trainer):
-        self.pokemon_trainer = pokemon_trainer
-        self.pokemon_number = randint(1, 1000)
-        Pokemon.pokemons[pokemon_trainer] = self
+    def __init__(self, trainer):
+        self.pokemon_trainer = trainer
+        self.number = randint(1, 1000)
+        self.name = self.get_name()
+        self.img = self.get_img()
+        self.height = self.get_height()
+        self.weight = self.get_weight()
+        self.level = 1
+        self.exp = 0
+        self.hp = randint(1,50)
+        self.power = randint(1,100)
+
+        self.rare = self.weight > 300
+
+        if self.rare:
+            self.level += 1
+            self.exp += 50
+
+        Pokemon.pokemons[trainer] = self
 
     def get_name(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.number}'
         response = requests.get(url)
         if response.status_code == 200:
-            return response.json()['forms'][0]['name']
+            return requests.get(url).json()['forms'][0]['name']
         else:
             return "Pikachu"
 
     def get_img(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.number}'
         response = requests.get(url)
         if response.status_code == 200:
-            return response.json()['sprites']['other']['official-artwork']['front_default']
+            return requests.get(url).json()['sprites']['other']['official-artwork']['front_default']
         else:
-            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            return "https://some-default.png"
 
     def get_height(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()['height']
-        else:
-            return 4
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.number}'
+        return requests.get(url).json().get('height', 4)
 
     def get_weight(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()['weight']
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.number}'
+        return requests.get(url).json().get('weight', 60)
+
+    def feed(self):
+        self.exp = self.exp + 10
+        if self.exp > 100:
+            self.level += 1
+            self.exp = 0
+            return "level up"
         else:
-            return 60
+            return "fed"
 
     def info(self):
-        return (f"Имя твоего покемона: {self.get_name()}\n"
+        rare_text = "🌟 Редкий покемон!" if self.rare else "Обычный покемон"
+        return (f"Имя твоего покемона: {self.name}\n"
                 f"Рост: {self.get_height() / 10} м\n"
-                f"Вес: {self.get_weight() / 10} кг")
+                f"Вес: {self.get_weight() / 10} кг\n"
+                f"{rare_text}\n"
+                f"Уровень: {self.level}, Опыт: {self.exp}\n"               
+                f"Сила: {self.power}\n"
+                f"hp: {self.hp}")
 
     def show_img(self):
-        return self.get_img()
+        return self.img
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+        
+
+class Wizard(Pokemon):
+    def info(self):
+        return 'У тебя покемон-волшебник' + super().info()
+    
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+    def info(self):
+        return 'У тебя покемон-боец' + super().info()
+    
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
